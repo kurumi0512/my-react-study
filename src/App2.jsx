7ec88334182080acdc3a42d1ac0e5cf903a3aa6c
unknown <foreverlove0512t@gmail.com>
2025-05-02 11:57:16 +0800

    Initial commit

---
+++ src/App2.jsx
@@ -0,0 +1,13 @@
+//JSX = javascript +HTML
+//App() 表示是一個react組件,字首字母要大寫
+
+function App() {
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <h2>micro-service</h2>
+        </div>
+    );
+}
+
+export default App;
