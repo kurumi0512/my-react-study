ea5be94e0d08c8c614aeafa4f4e3c6e3a9855a67
unknown <foreverlove0512t@gmail.com>
2025-05-02 14:36:49 +0800

    250502 14:36

---
+++ src/App8.jsx
@@ -0,0 +1,62 @@
+/*
+商品資料如下:
+{ id: 1, name: '蘋果', price: 40, category: '水果' },
+{ id: 2, name: '洗髮精', price: 120, category: '日用品' },
+{ id: 3, name: '香蕉', price: 55, category: '水果' },
+{ id: 4, name: '牙膏', price: 45, category: '日用品' }
+請利用 react 將上述商品資料透過 jsx + <table> 標籤呈現
+*/
+
+function App() {
+    // 物件陣列
+    const products = [
+        { id: 1, name: '蘋果', price: 40, category: '水果', qty:2},
+        { id: 2, name: '洗髮精', price: 120, category: '日用品', qty:4 },
+        { id: 3, name: '香蕉', price: 55, category: '水果', qty:6 },
+        { id: 4, name: '牙膏', price: 45, category: '日用品', qty:8 }
+    ];
+
+    //計算價格總和,reduce是歸納,sum代表前一次累計的紀錄,會做4次,sum+product.price第一次是40,第二次是40+120
+    const totalPrice = products.reduce((sum, product,qty) => sum +(product.price*product.qty), 0);
+    return (
+       <>
+        <h1>商品列表</h1>
+       <table border= "1">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>商品名稱</th>
+                    <th>價格</th>
+                    <th>數量</th>
+                    <th>小計</th>
+                    <th>類別</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    products.map((product)=>(
+                        <tr key={product.id}>
+                            <td>{product.id}</td>
+                            <td>{product.name}</td>
+                            <td align="right">{product.price}</td>
+                            <td align="right">{product.qty}</td>
+                            <td align="right">{product.price * product.qty}</td>
+                            <td>{product.category}</td>
+                        </tr>
+                    ))
+                }
+
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colSpan="4" align="right">總計</td>
+                    <td align="right">{totalPrice}</td>
+                    <td></td>
+                </tr>
+            </tfoot>
+        </table>
+        </>
+    )
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import App3 from "./App3.jsx";
 import App4 from "./App4.jsx";
 import App5 from "./App5.jsx";
 import App6 from "./App6.jsx";
+import App7 from "./App7.jsx";
+import App8 from "./App8.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App6 />
+        <App8 />
     </React.StrictMode>
 );
