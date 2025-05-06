360bc2edc75840f4bd25830b029b240fe8da3167
kurumi0512 <foreverlove0512t@gmail.com>
2025-05-06 11:25:20 +0800

    250506 11:25

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import App14 from "./App14.jsx";
 import App15 from "./App15.jsx";
 import TodoList5 from "./TodoList5.jsx";
-import Cart2 from "./Cart2.jsx";
+import Cart3 from "./Cart3.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Cart2 />
+        <Cart3 />
     </React.StrictMode>
 );
